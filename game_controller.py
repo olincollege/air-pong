8781d5bd01c96ed3df5c93a16738d7be8ac9f363
julakshah <julakshah@gmail.com>
2025-04-20 20:32:18 -0400

    added game controller and pylint whitelist

---
+++ game_controller.py
@@ -0,0 +1,12 @@
+from air_pong_controller import PongController
+
+
+def main():
+    """begins and runs the game"""
+    controller = PongController()
+    while True:
+        v = 1
+
+
+if __name__ == "__main__":
+    main()
