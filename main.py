6f488bf28d53a6edae8faf5ed9dc436debf0d903
Lukas Littlejohn <LLittlejohn@olin.edu>
2025-05-06 02:43:11 -0400

    controller.release() no longer exists, fixed

--- main.py
+++ main.py
@@ -9,36 +9,24 @@ from air_pong_model import PongModel
 def main():
     """Run the air-pong game"""
 
-    # intialize MVCC (2 controllers)
+    # initialize MVCC (2 controllers)
     model = PongModel(2, 11)
-<<<<<<< HEAD
     controller = PongController(model)
     screen = pygame.display.set_mode((1500, 600))
     view = PongView(screen, model)
     view.prepare_images()
-=======
-    controller1 = PongController(model)
-    view = PongView()
-    view.prepare_images(model, screen)
->>>>>>> 75f530476896b0aeb87710744f0a11ef025a49ee
 
     # main loop to run code
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # pylint: disable=no-member
-                controller.release()
                 running = False
 
-<<<<<<< HEAD
         controller.update_hand()
-=======
-        controller1.update_hand()
->>>>>>> 75f530476896b0aeb87710744f0a11ef025a49ee
         model.trajectory()
         view.display()
         if model.check_win() is not False:
-            controller.release()
             while running:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:  # pylint: disable=no-member
